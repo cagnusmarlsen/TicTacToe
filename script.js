3ff75d1561158d82eb4e8c1860ca2d35b85133c9
cagnusmarlsen <0arunabh30@gmail.com>
2023-01-25 10:17:44 +0530

    Can place marks now

--- script.js
+++ script.js
@@ -1,3 +1,43 @@
-const gameBoard = (() =>{
-    const board = [];
+const cells = document.querySelectorAll(".board");
+
+const gameBoard = () => {
+    const board = ["","","","","","","","",""];
+    console.log(board);
+    return {board};
+}
+
+const newBoard = gameBoard();
+const a =newBoard.board;
+
+displayBoard();
+
+function displayBoard () {
+    for(let i = 0; i < 9; i++) {
+        cells[i].textContent = a[i];
+    }
+}
+let turn = 1;
+function check (e) {
+    let cell = e.target.id;
+    if(turn == 1) {
+       addX(cell);
+       turn++;
+    }
+    else {
+        addO(cell);
+        turn--;
+    }
+}
+
+cells.forEach(cells => {
+    cells.addEventListener('click', check);
 })
+
+function addX (cell) {
+    a[cell] = "X";
+    displayBoard();
+}
+function addO (cell) {
+    a[cell] = "O";
+    displayBoard();
+}
